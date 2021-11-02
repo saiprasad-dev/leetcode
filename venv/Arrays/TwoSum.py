5b2f2c73e9f63eeed59eaa93fa2d8380c251811a
saimadhuri <sai.d.prasad@gmail.com>
2021-11-01 21:55:18 -0700

    Brute force way of solving Two Sum problem

---
+++ venv/Arrays/TwoSum.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        """
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+        :param nums: [2,7,11,15]
+        :param target: 9
+        :return: 0,1
+        """
+
+        for i in range (0,len(nums)-1):
+            for j in range (i+1,len(nums)):
+                if target == nums[i] + nums[j]:
+                    return [i,j]
+
